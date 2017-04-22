ef97762b8a45389e5c750390e0048f9a3cfca527
Allon Hadaya <self@allon.nyc>
2017-04-22 17:35:18 -0400

    Initial size checker implementation counts the @angular/* packages mentioned in the quickstart guide.

---
+++ README.md
@@ -0,0 +1,9 @@
+# angular-size-checker
+
+An attempt at measuring the size of the angular framework.
+
+```sh
+> npm install
+> node index.js
+```
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const { join } = require('path');
+const { readFileSync } = require('fs');
+const { gzipSync } = require('zlib');
+
+const angularPackages = Object.keys(require('./package.json').dependencies).filter(p => p.indexOf('@angular') === 0);
+
+console.log('\nreading minified sub-packages...\n');
+
+const minified = angularPackages.map(package => {
+
+  const name = package.split('/')[1];
+  const path = join('.', 'node_modules', package, 'bundles', `${name}.umd.min.js`);
+  const buffer = readFileSync(path);
+
+  console.log(`${package} :: ${buffer.length}`);
+  return buffer;
+});
+
+const concatenated = Buffer.concat(minified);
+
+console.log('\nconcatenating and compressing...\n');
+
+const gzipped = gzipSync(concatenated);
+
+const size = gzipped.length;
+
+console.log(`compressed :: ${size}`)
